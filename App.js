2cd08d787efe86b102af68e9e0f7dadd2af01318
OFERR <oferr@amdocs.com>
2024-04-12 23:49:52 +0300

    Barometer Issue iOS 17.4

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Platform } from 'react-native';
+import { Barometer } from 'expo-sensors';
 
 export default function App() {
+  const [{ pressure, relativeAltitude }, setData] = useState({ pressure: 0, relativeAltitude: 0 });
+  const [subscription, setSubscription] = useState(null);
+
+  const toggleListener = () => {
+    subscription ? unsubscribe() : subscribe();
+  };
+
+  const subscribe = () => {
+    setSubscription(Barometer.addListener(setData));
+  };
+
+  const unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.wrapper}>
+        <Text>Barometer: Listener {subscription ? 'ACTIVE' : 'INACTIVE'}</Text>
+        <Text>Pressure: {pressure} hPa</Text>
+        <Text>
+          Relative Altitude:{' '}
+          {Platform.OS === 'ios' ? `${relativeAltitude} m` : `Only available on iOS`}
+        </Text>
+        <TouchableOpacity onPress={toggleListener} style={styles.button}>
+          <Text>Toggle listener</Text>
+        </TouchableOpacity>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  button: {
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+    marginTop: 15,
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
   },
 });
